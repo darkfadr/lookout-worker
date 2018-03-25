59709df12c9d5caa70734167cd37e54ccf28faab
Ashley Narcisse-Austin <Ashley_NarcisseAustin@ultimatesoftware.com>
2018-03-25 10:14:51 -0400

    sanity checks

--- src/worker.js
+++ src/worker.js
@@ -7,19 +7,17 @@ const plate = axios.create({baseURL, timeout: 150000});
 
 
 rabbit.on('ready', () => {
+  connection.queue('screenshot', function (q) {
+      // Catch all messages
+      q.bind(BIND_KEY);
+      // Receive messages
+      q.subscribe(message => {
+        log.debug('\n\nReceived: ', message)
 
-    connection.queue('screenshot', function (q) {
-        // Catch all messages
-        q.bind(BIND_KEY);
-        // Receive messages
-        q.subscribe(message) => {
-          log.debug('\n\nReceived: ', message)
-
-          plate.post('/detect', {})
-            .then(res => log.debug(res.data))
-            .catch(err => log.error(`faile dto parse message in queue`));
-        });
-    });
+        plate.post('/detect', {})
+          .then(res => log.debug(res.data))
+          .catch(err => log.error(`faile dto parse message in queue`));
+      });
   });
 });
 
